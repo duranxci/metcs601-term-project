2b66eefe79de29c3c1e166c5b804a03541d3bf44
duranxci <duranxci@gmail.com>
2023-11-28 19:48:48 -0500

    initial src code

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function getHeader(logo) {
+  return `
+      <header>
+      <div class="banner">
+        <a href="./index.html">
+          <img src="./assets/images/${logo}" alt="A game controller joystick" />
+        </a>
+        <h1 id="page-title">About</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="./index.html">Home</a>
+          </li>
+          <li>
+            <a href="./portfolio.html">Portfolio</a>
+          </li>
+          <li>
+            <a href="./about.html">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  `;
+}
